11e97b3005e73b13f5841201a28f2b69297e80b1
bobororomeme <borrome.niclot@saint-thom.fr>
2023-09-07 21:31:48 +0200

    -

---
+++ README.md
@@ -0,0 +1,18 @@
+
+> *Note: The code isn't clean, the english isn't error-free, it's in its very early stage.*
+
+# Raygato.py and tmpc.py
+
+These are two little utilities that I made and that I use a lot, so I thought I'd share.
+
+## tmpc.py
+
+> A simple python script to play quickly with code without worrying about setting up stuff.
+
+That's where it all started. I really liked making a lot of little projects that I finished in hours, but setting up everything was usually very time-costing and I sometime lost the will to finish these little projects. I wanted some way to spawn all the things I needed, quickly, without needing any internet access, and simple enough so that I could add more templates and edit that script. So I wrote very quickly that python script 'tmpc.py' (very poorly written, at least in the first commits), which could enable me to do exactly what I needed.
+
+
+## Raygato.py
+
+Raygato (RayGaTo : Raylib Game Tool) is a shell assistant for game developement.
+
--- tmpc.py
+++ tmpc.py
@@ -1,16 +1,64 @@
 VERSION = "2"
 MAX_FILES = 10_000
 
+def scope(f):
+	f()
+	return f
+
 from os import listdir, mkdir, system, remove, path
 from random import randint
 from time import sleep
+import json
+import platform
+import re
 print(f"==== TEMP-CODE (v{VERSION}) ====")
 __dir__ = __file__.replace("\\","/").rsplit("/",1)[0]
 templates = listdir(f"{__dir__}/templates/")
+
+
+filters_data = json.load(open(f"{__dir__}/osfilters.json"))
+
+filt_funcs = {}
+def filt_func(name: str):
+	def wrapper(f):
+		filt_funcs[name] = f
+		return f
+	return wrapper
+
+filt_func("startswith")(str.startswith)
+filt_func("endswith")(str.endswith)
+@filt_func("regex")
+def filter_regex(inp: str, regx: str):
+	return bool(re.match(regx, inp))
+
+@scope
+def filter_templates():
+	# Store all exclusion filters
+	exclude_thens = []
+	for i in filters_data:
+		for cond, output in i["if"].items():
+			if getattr(platform, cond)() == output:
+				break
+		else:
+			exclude_thens.append(i["then"])
+	rmv_ls = []
+	# Apply exclusion filters
+	for t in templates:
+		for excl in exclude_thens:
+			for filt_name, filt_f in filt_funcs.items():
+				if filt_name in excl and filt_f(t, excl[filt_name]):
+					rmv_ls.append(t)
+					break
+			else:
+				continue
+			break
+	for i in rmv_ls:
+		templates.remove(i)
+
+
 for n,i in enumerate(templates):
 	print(f"{n}. {i}")
 cmd = None
-
 while not cmd:
 	cmd = input("Use template or extension ([0-9]+ for templates, a dot then the extension for single files):").replace("\"", "\\\"")
 
